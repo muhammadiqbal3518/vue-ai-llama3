c508146d7da0381a21da2a6a248ef6f00cb58fb5
muhammadiqbal3518 <141794200+muhammadiqbal3518@users.noreply.github.com>
2024-12-18 19:23:03 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -232,48 +232,8 @@ export default {
       class="fixed bottom-0 left-0 w-full shadow-md p-4 flex items-center gap-2"
     >
       <!-- Upload Audio File -->
-      <input
-        type="file"
-        accept="audio/*"
-        @change="handleFileUpload"
-        id="audio-upload"
-        class="hidden"
-        :disabled="content || audioBlob"
-      />
-      <label
-        for="audio-upload"
-        class="cursor-pointer flex items-center justify-center bg-gray-200 hover:bg-gray-300 text-gray-600 w-10 h-10 rounded-full"
-        :disabled="audioFile || content"
-      >
-        <img src="../public/upload.png" alt="Upload" />
-      </label>
 
       <!-- Rekam Suara -->
-      <button
-        type="button"
-        @click="isRecording ? stopRecording() : startRecording()"
-        :class="
-          isRecording ? 'bg-red-500' : 'bg-gray-200 hover:bg-gray-300 text-gray-600'
-        "
-        class="flex items-center justify-center w-10 h-10 rounded-full"
-      >
-        <img v-if="!isRecording" src="../public/microphone.png" alt="Microphone" />
-        <svg
-          v-else
-          xmlns="http://www.w3.org/2000/svg"
-          class="w-6 h-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M6 18L18 6M6 6l12 12"
-          />
-        </svg>
-      </button>
 
       <!-- Input Teks -->
       <input
@@ -290,26 +250,8 @@ export default {
         :disabled="processing"
         class="bg-green-500 hover:bg-green-600 py-2 px-4 text-white font-bold rounded-md"
       >
-        <div v-if="processing" role="status">
-          <svg
-            aria-hidden="true"
-            class="w-4 h-4 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-            viewBox="0 0 100 101"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-              fill="currentColor"
-            />
-            <path
-              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-              fill="currentFill"
-            />
-          </svg>
-          <span class="sr-only">Loading...</span>
-        </div>
-        <img v-else src="../public/send.png" alt="Send" />
+        <div v-if="processing" role="status">...</div>
+        <div v-else>Send</div>
       </button>
       <div
         @click="cleanChat"
